c46dfc69c8f0642acbd27094b378de356992a6cd
eithar2751998 <eithar.saeed256@gmail.com>
2023-02-17 14:16:27 -0800

    tak7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 1 to 9 .
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char ch;
+    
+    for (ch = 'z'; ch >= 'a'; ch--)
+        putchar(ch);
+		
+	putchar('\n');
+
+	return (0);
+}
