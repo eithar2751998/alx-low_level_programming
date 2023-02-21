e804aeb96e4f8f2a059ea298f97ce8ccb7247ffc
eithar2751998 <eithar.saeed256@gmail.com>
2023-02-21 12:50:06 -0800

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Print 10 times the alphabet, in lowercase, followed by a new line.
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	     reurn (1);
+	else
+		return (0);
+}
