f79670b7a5d03c4b454534609a40d04b87832ffa
eitharSaeed <31311130+eithar2751998@users.noreply.github.com>
2023-02-21 22:26:34 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 include "main.h"
 
 /**
- * main - Print 10 times the alphabet, in lowercase, followed by a new line.
+ * print_alphabet_x10 - Print 10 times the alphabet, in lowercase, followed by a new line.
  * Return: Always 0
  */
 void print_alphabet_x10(void)
