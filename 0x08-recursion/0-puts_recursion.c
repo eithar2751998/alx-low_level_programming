1a37ba0c5edce765b09d1a04addbdbd80aacff1c
eitharSaeed <31311130+eithar2751998@users.noreply.github.com>
2023-03-11 14:57:27 +0200

    tasks

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _puts_recursion - print a string
- * @s:string
- * Return:void
- */
+* _puts_recursion - print a string
+* @s:string
+* Return:void
+*/
 void _puts_recursion(char *s)
 {
 if (*s)
